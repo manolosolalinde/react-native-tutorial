5009f70f237a9650610fda7b8a7e6bccffc92bdd
Manuel Solalinde <manolosolalinde@gmail.com>
2021-04-07 11:41:43 -0400

    changed configs

--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
-import { globalStyles, images } from '../styles/global';
-import Card from '../shared/card';
+import { StyleSheet, View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails({ navigation }) {
-  const rating = navigation.getParam('rating');
-  
+export default function ReviewDetails({ route,navigation }) {
+  const { title,body,rating } = route.params;
   return (
     <View style={globalStyles.container}>
-      <Card>
-        <Text style={globalStyles.titleText}>
-          { navigation.getParam('title') }
-        </Text>
-        <Text>{ navigation.getParam('body') }</Text>
-        <View style={styles.rating}>
-          <Text>GameZone rating: </Text>
-          <Image source={images.ratings[rating]} />
-        </View>
-      </Card>
+      <Text>{ title}</Text>
+      <Text>{ body }</Text>
+      <Text>{ rating }</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  rating: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    paddingTop: 16,
-    marginTop: 16,
-    borderTopWidth: 1,
-    borderTopColor: '#eee',
-  }
-});
